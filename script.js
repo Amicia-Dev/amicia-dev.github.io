391e9ec5f78942ea4afa7bfea5bb7637681b4862
Amicia <143362445+Amicia-Dev@users.noreply.github.com>
2024-09-05 23:29:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const parallaxContent = document.querySelector('.parallax-content');
+
+document.addEventListener('mousemove', (event) => {
+    const xAxis = (window.innerWidth / 2 - event.pageX) / 25; /* Adjust the divisor for speed */
+    const yAxis = (window.innerHeight / 2 - event.pageY) / 25; /* Adjust the divisor for speed */
+
+    parallaxContent.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+document.addEventListener('mouseleave', () => {
+    parallaxContent.style.transform = 'rotateY(0deg) rotateX(0deg)';
+});
